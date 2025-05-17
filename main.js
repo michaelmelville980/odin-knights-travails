eaf19bb018a9645171aab8873fd0f3902b12cfe7
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-17 13:01:38 -0400

    finished implementation

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-/* Chess board is 7x7 and is represented by [x,y] coordinates where (x,y >= 0 && x,y <= 7)
- * Knight movement could be represented via 8 different movements (e.g., [x+1, y+2], etc.)
- * Each move would need to be checked to make sure it stays in [x,y] coordinates
- */
-const knightMoves = function (start = [3, 3], end = [4, 5]) {
-  /* Generating All Possible Movements (e.g., [1,2] = Up 1, Right 2) */
+const knightMoves = function (start = [], end = []) {
   const allMovements = [
     [1, 2],
     [2, 1],
@@ -15,23 +10,28 @@ const knightMoves = function (start = [3, 3], end = [4, 5]) {
     [-1, 2],
   ];
 
-  /* Store Movement Paths */
-  let movementsQueue = [[3, 3]];
+  let queue = [{ position: start, path: [start] }];
 
-  /* Checking Movement Paths (BFS) until optimal path found */
-  while (movementsQueue > 0) {
-    for (let i = 0; i < allMovements.length; i++) {
-      /* Generates Movement */
-      const [dx, dy] = allMovements[i];
-      const x = start[0] + dx;
-      const y = start[1] + dy;
+  while (queue.length > 0) {
+    let entry = queue.shift();
 
-      /* Checks if Movement is Valid (within Board) */
-      if (x <= 7 || x >= 0 || y <= 7 || y >= 0) {
-        movementsQueue;
+    if (entry.position[0] === end[0] && entry.position[1] === end[1]) {
+      return entry.path;
+    }
+
+    for (const [dx, dy] of allMovements) {
+      const futureMovement = [entry.position[0] + dx, entry.position[1] + dy];
+      if (
+        futureMovement[0] <= 7 &&
+        futureMovement[0] >= 0 &&
+        futureMovement[1] <= 7 &&
+        futureMovement[1] >= 0
+      ) {
+        const newPath = entry.path.concat([futureMovement]);
+        queue.push({position: futureMovement, path: newPath});
       }
     }
   }
-
-  const moveOne = [start[0] + 1, start[1] + 2];
 };
+
+console.log(knightMoves([0,0],[6,4]));
